18ed6b82b8f64b9f300b2cef1d715e85da545958
dafneg12 <s23120167@alumnos.itsur.edu.mx>
2025-07-04 15:20:07 -0600

    hhh

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,31 +29,57 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
-            label1.Font = new Font("Gabriola", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 9);
+            label1.Font = new Font("Candara", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 3);
             label1.Name = "label1";
-            label1.Size = new Size(329, 62);
+            label1.Size = new Size(273, 50);
             label1.TabIndex = 0;
             label1.Text = "Residecia arboledas";
             label1.Click += label1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(106, 156, 253);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(856, 53);
+            panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.LightSteelBlue;
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 53);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(191, 346);
+            panel2.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(506, 349);
-            Controls.Add(label1);
+            ClientSize = new Size(856, 399);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Label label1;
+        private Panel panel1;
+        private Panel panel2;
     }
 }
